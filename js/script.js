1f9747d12eb67b7edbdb47b2088a5eaeab996629
DarioLoForti <lofortidario@gmail.com>
2023-11-17 15:11:49 +0100

    exercise completetd

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let nome = prompt("inserisci il tuo nome");
+let cognome = prompt("inserisci il tuo cognome");
+let colore = prompt("insrisci il tuo colore preferito");
+// let text = nome + cognome + colore + 23;
+document.getElementById("user").innerHTML = `${nome} ${cognome} ${colore} 23`;
